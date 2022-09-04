4d17708a971601b994fd2267a6f28a1862fb23fe
ryugenxd <anonimous7770@gmail.com>
2022-09-04 18:01:17 +0800

    + fiture Change password

--- users/app/asset/inc/inc_setting.php
+++ users/app/asset/inc/inc_setting.php
@@ -1,57 +1,57 @@
 <div class="container" style="width: 100%; display: flex; justify-content: center; align-items: center;">
-    <div class="setting shadow-sm">
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">
-                <a type="button" data-bs-toggle="modal" data-bs-target="#password" class="btn">Change password</a>
-            </li>
-            <li class="list-group-item">
-                <a type="button" data-bs-toggle="modal" data-bs-target="#username" class="btn">Change username</a>
-            </li>
-            <li class="list-group-item">
-                <a href="" class="btn">Delete account</a>
-            </li>
-            <li class="list-group-item">
-                <a href="http://wa.me/6283873614764/" class="btn">Reporting bug!</a>
-            </li>
-            <li class="list-group-item">
-                <a href="" class="btn">Service</a>
-            </li>
-            <li class="list-group-item">
-                <a type="button" data-bs-toggle="modal" data-bs-target="#confirm" class="btn btn-danger">Logout</a>
-            </li>
-        </ul>
-    </div>
+   <div class="setting shadow-sm">
+      <ul class="list-group list-group-flush">
+         <li class="list-group-item">
+            <a type="button" data-bs-toggle="modal" data-bs-target="#password" class="btn">Change password</a>
+         </li>
+         <li class="list-group-item">
+            <a type="button" data-bs-toggle="modal" data-bs-target="#username" class="btn">Change username</a>
+         </li>
+         <li class="list-group-item">
+            <a href="" class="btn">Delete account</a>
+         </li>
+         <li class="list-group-item">
+            <a href="http://wa.me/6283873614764/" class="btn">Reporting bug!</a>
+         </li>
+         <li class="list-group-item">
+            <a href="" class="btn">Service</a>
+         </li>
+         <li class="list-group-item">
+            <a type="button" data-bs-toggle="modal" data-bs-target="#confirm" class="btn btn-danger">Logout</a>
+         </li>
+      </ul>
+   </div>
 </div>
 
 <style>
-    .setting {
-        width: 300px;
-        height: auto;
-        border-radius: 10px;
-        box-sizing: border-box;
-        box-shadow: 3px 3px 3px white;
-        background-color: #FFFFFF;
-        padding: 5px;
-    }
+   .setting {
+      width: 300px;
+      height: auto;
+      border-radius: 10px;
+      box-sizing: border-box;
+      box-shadow: 3px 3px 3px white;
+      background-color: #FFFFFF;
+      padding: 5px;
+   }
 </style>
 
 
 <!-- modal logout -->
 <div class="modal fade" id="confirm" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">confirm</h5>
-            </div>
-            <div class="modal-body">
-                <h4 class="text-center">do you want to go out?</h4>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">No</button>
-                <a href="../logout.php" class="btn btn-primary">Yes</a>
-            </div>
-        </div>
-    </div>
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">confirm</h5>
+         </div>
+         <div class="modal-body">
+            <h4 class="text-center">do you want to go out?</h4>
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">No</button>
+            <a href="../logout.php" class="btn btn-primary">Yes</a>
+         </div>
+      </div>
+   </div>
 </div>
 
 <!-- ============================ change username ======================= -->
@@ -59,88 +59,127 @@
 <?php
 
 if (isset($_POST['gantiUsername'])) {
-    if (changeUsername($_POST) > 0) {
-        echo "
+   if (changeUsername($_POST) > 0) {
+      echo "
         <div class='container' style='position:fixed; z-index:999999; top:0;' id='alert'>
         <div class='alert alert-primary d-flex justify-content-around' role='alert'>
-            <p>Change username success!</p>        
+            <p>Change username success!</p>
             <button type='button' onclick='closeAlert();' class='btn-close'  aria-label='Close'></button>
         </div>
         </div>
         ";
-    } else {
-        echo "
+   } else {
+      echo "
     <div class='container' style='position:fixed; z-index:999999; top:0;' id='alert'>
     <div class='alert alert-primary d-flex justify-content-around' role='alert'>
-        <p>Change username not success!</p>        
+        <p>Change username not success!</p>
         <button type='button' onclick='closeAlert();' class='btn-close'  aria-label='Close'></button>
     </div>
     </div>
     ";
-    }
+   }
 }
 
 ?>
 
+<!--Logic password -->
+<?php
+if (isset($_POST['gantiPassword'])) {
+   if ($_POST['password'] == $_POST['passwordBaru']) {
+      if (changePassword($_POST) > 0) {
+         echo "
+        <div class='container' style='position:fixed; z-index:999999; top:0;' id='alert'>
+        <div class='alert alert-primary d-flex justify-content-around' role='alert'>
+            <p>Change password  success!</p>
+            <button type='button' onclick='closeAlert();' class='btn-close'  aria-label='Close'></button>
+        </div>
+        </div>
+        ";
+      } else {
+         echo "
+    <div class='container' style='position:fixed; z-index:999999; top:0;' id='alert'>
+    <div class='alert alert-primary d-flex justify-content-around' role='alert'>
+        <p>Change password not success!</p>
+        <button type='button' onclick='closeAlert();' class='btn-close'  aria-label='Close'></button>
+    </div>
+    </div>
+    ";
+      }
+   } else {
+      echo "
+    <div class='container' style='position:fixed; z-index:999999; top:0;' id='alert'>
+    <div class='alert alert-primary d-flex justify-content-around' role='alert'>
+        <p>Confirm password is not the same as password !</p>
+        <button type='button' onclick='closeAlert();' class='btn-close'  aria-label='Close'></button>
+    </div>
+    </div>
+    ";
+
+   }
+}
+?>
+
+
 <!-- modal change username -->
 <div class="modal fade" id="username" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="11" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Change username!</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="" method="post">
-                    <!-- input link demo -->
-                    <div class="mb-3">
-                        <input type="hidden" name="usernameLama" value="<?= $user['username']; ?>">
-                        <input type="hidden" name="id" value="<?= $user['id']; ?>">
-                        <label for="Kategori" class="form-label text-danger">New username!</label>
-                        <input type="text" id="Kategori" name="usernameBaru" class="form-control mb-2">
-                    </div>
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Change username!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+         </div>
+         <div class="modal-body">
+            <form action="" method="post">
+               <!-- input link demo -->
+               <div class="mb-3">
+                  <input type="hidden" name="usernameLama" value="<?= $user['username']; ?>">
+                  <input type="hidden" name="id" value="<?= $user['id']; ?>">
+                  <label for="Kategori" class="form-label text-danger">New username!</label>
+                  <input type="text" id="Kategori" name="usernameBaru" class="form-control mb-2">
+               </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" name="gantiUsername" class="btn btn-primary">Save</button>
+               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+               <button type="submit" name="gantiUsername" class="btn btn-primary">Save</button>
             </div>
-            </form>
-        </div>
-    </div>
+         </form>
+      </div>
+   </div>
 </div>
 
 <!-- modal change password -->
 <div class="modal fade" id="password" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="11" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Change password!</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p class="text-center text-danger">Belom beres!!</p>
-                <form action="">
-                    <div class="mb-3">
-                        <label for="Kategori" class="form-label text-danger">Password!</label>
-                        <input type="text" id="Kategori" name="Kategori" class="form-control mb-2">
-                    </div>
-                    <div class="mb-3">
-                        <label for="Kategori" class="form-label text-danger">New password!</label>
-                        <input type="text" id="Kategori" name="Kategori" class="form-control mb-2">
-                    </div>
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Change password!</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+         </div>
+         <div class="modal-body">
+            <p class="text-center text-danger">
+               Versi Beta 🤣
+            </p>
+            <form action="" method="post">
+               <input type="hidden" name="id" value="<?= $user['id']; ?>">
+               <div class="mb-3">
+                  <label for="Kategori" class="form-label text-danger">New Password!</label>
+                  <input type="text" id="password" name="password" class="form-control mb-2" autocomplete="off" required>
+               </div>
+               <div class="mb-3">
+                  <label for="Kategori" class="form-label text-danger">Confirm password!</label>
+                  <input type="text" id="passwordBaru" name="passwordBaru" class="form-control mb-2" autocomplete="off" required>
+               </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save</button>
+               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+               <button type="submit" name="gantiPassword" class="btn btn-primary">Save</button>
             </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<script>
-    function closeAlert() {
-        let notif = document.getElementById("alert");
-        notif.setAttribute("style", "display:none;")
-    }
-</script>
+         </form>
+      </div>
+   </div>
+   <script>
+      function closeAlert() {
+         let notif = document.getElementById("alert");
+         notif.setAttribute("style", "display:none;")
+      }
+   </script>
--- users/app/sistem/functions.php
+++ users/app/sistem/functions.php
@@ -1,204 +1,217 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 require "../admin/sistem/conn.php";
-// edit images profile 
-function editImages($data)
-{
-    global $conn;
+// edit images profile
+function editImages($data) {
+   global $conn;
 
-    $images = upload();
-    $id  = $data['id'];
+   $images = upload();
+   $id = $data['id'];
 
-    $query = "UPDATE multi_user SET img = '$images' WHERE id = $id";
+   $query = "UPDATE multi_user SET img = '$images' WHERE id = $id";
 
-    mysqli_query($conn, $query);
+   mysqli_query($conn, $query);
 
-    return mysqli_affected_rows($conn);
+   return mysqli_affected_rows($conn);
 }
 
 // function upload
-function upload()
-{
-    $namaFile = $_FILES['images']['name'];
-    $ukuranFile = $_FILES['images']['size'];
-    $error = $_FILES['images']['error'];
-    $tmpName = $_FILES['images']['tmp_name'];
+function upload() {
+   $namaFile = $_FILES['images']['name'];
+   $ukuranFile = $_FILES['images']['size'];
+   $error = $_FILES['images']['error'];
+   $tmpName = $_FILES['images']['tmp_name'];
 
-    // cek apakah tidak ada gambar yang diupload
-    if ($error === 4) {
-        echo "<div class='alert alert-danger' role='alert' style='position:fixed; z-index:9999999; top:0;'>
+   // cek apakah tidak ada gambar yang diupload
+   if ($error === 4) {
+      echo "<div class='alert alert-danger' role='alert' style='position:fixed; z-index:9999999; top:0;'>
         Pilih gambar terlebih dahulu!!!
         </div>";
-        return false;
-    }
+      return false;
+   }
 
-    // cek apakah yang diupload adalah gambar
-    $ekstensiGambarValid = ['jpg', 'jpeg', 'png', "gif"];
-    $ekstensiGambar = pathinfo($namaFile, PATHINFO_EXTENSION);
-    if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
-         echo "<div class='alert alert-danger' role='alert' id='alert'  style='position:fixed; z-index:9999999; top:0;'>
+   // cek apakah yang diupload adalah gambar
+   $ekstensiGambarValid = ['jpg',
+      'jpeg',
+      'png',
+      "gif"];
+   $ekstensiGambar = pathinfo($namaFile, PATHINFO_EXTENSION);
+   if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
+      echo "<div class='alert alert-danger' role='alert' id='alert'  style='position:fixed; z-index:9999999; top:0;'>
           $ekstensiGambar bukan gambar! ekstensiGambarValid ". implode(", ", $ekstensiGambarValid)."
           </div>";
-          return false;
-    }
+      return false;
+   }
 
-    // cek jika ukurannya terlalu besar
+   // cek jika ukurannya terlalu besar
 
-    if ($ukuranFile > 1000000) {
-        echo "<div class='alert alert-danger' id='alert' role='alert' style='position:fixed; z-index:9999999; top:0;'>
+   if ($ukuranFile > 1000000) {
+      echo "<div class='alert alert-danger' id='alert' role='alert' style='position:fixed; z-index:9999999; top:0;'>
         ukuran images terlalu besar!
         maximum ukuranFile '1mb'
         </div>";
-        return false;
-    }
+      return false;
+   }
 
-    if ($ukuranFile  < 20000) {
-        echo "<div class='alert alert-danger' id='alert' role='alert' style='position:fixed; z-index:9999999; top:0;'>
+   if ($ukuranFile < 20000) {
+      echo "<div class='alert alert-danger' id='alert' role='alert' style='position:fixed; z-index:9999999; top:0;'>
         ukuran images terlalu kecil!
         minimum ukuranFile '200kb'
         </div>";
-        return false;
-    }
-
-    // lolos pengecekan, gambar siap diupload
-
-    // generate nama gambar baru
-    $namaFileBaru = uniqid().".".$ekstensiGambar;
-    $dest = "../admin/images-post/".$namaFileBaru;
-    
-    /* cek bisa di upload nggak */
-    if (!move_uploaded_file($tmpName, $dest)) {
       return false;
-    }
+   }
 
-    return $namaFileBaru;
+   // lolos pengecekan, gambar siap diupload
+
+   // generate nama gambar baru
+   $namaFileBaru = uniqid().".".$ekstensiGambar;
+   $dest = "../admin/images-post/".$namaFileBaru;
+
+   /* cek bisa di upload nggak */
+   if (!move_uploaded_file($tmpName, $dest)) {
+      return false;
+   }
+
+   return $namaFileBaru;
 }
 
 
 // funtion editProfile
-function editProfile($data)
-{
-    global $conn;
-    $name = $data["name"];
-    $addres = htmlspecialchars($data['addres']);
-    $email = htmlspecialchars($data['email']);
-    $nophone = htmlspecialchars($data['nophone']);
-    $old = htmlspecialchars($data['old']);
+function editProfile($data) {
+   global $conn;
+   $name = $data["name"];
+   $addres = htmlspecialchars($data['addres']);
+   $email = htmlspecialchars($data['email']);
+   $nophone = htmlspecialchars($data['nophone']);
+   $old = htmlspecialchars($data['old']);
 
-    $query = "UPDATE profile SET name = '$name', address = '$addres', email = '$email', nophone = '$nophone', old = '$old' WHERE name = '$name'";
+   $query = "UPDATE profile SET name = '$name', address = '$addres', email = '$email', nophone = '$nophone', old = '$old' WHERE name = '$name'";
 
-    mysqli_query($conn, $query);
+   mysqli_query($conn, $query);
 
-    return mysqli_affected_rows($conn);
+   return mysqli_affected_rows($conn);
 }
 
 
 // function posting
-function posting($data)
-{
-    global $conn;
-    $title = htmlspecialchars($data['title']);
-    $demo = htmlspecialchars($data['demo']);
-    $source = htmlspecialchars($data['source']);
-    $deskripsi = htmlspecialchars($data['deskripsi']);
-    $author = htmlspecialchars($data['author']);
-    $date = htmlspecialchars($data['date']);
-    $kategori = htmlspecialchars($data['kategori']);
-    $images = upload();
-    if (!$images) {
-        return false;
-    }
+function posting($data) {
+   global $conn;
+   $title = htmlspecialchars($data['title']);
+   $demo = htmlspecialchars($data['demo']);
+   $source = htmlspecialchars($data['source']);
+   $deskripsi = htmlspecialchars($data['deskripsi']);
+   $author = htmlspecialchars($data['author']);
+   $date = htmlspecialchars($data['date']);
+   $kategori = htmlspecialchars($data['kategori']);
+   $images = upload();
+   if (!$images) {
+      return false;
+   }
 
-    $query = "INSERT INTO postingan VALUES(NULL, '$title', '$demo', '$source', '$images', '$deskripsi', '$author', '$date', '$kategori')";
+   $query = "INSERT INTO postingan VALUES(NULL, '$title', '$demo', '$source', '$images', '$deskripsi', '$author', '$date', '$kategori')";
 
-    $post = mysqli_query($conn, $query);
+   $post = mysqli_query($conn, $query);
 
-    if ($post) {
-        mysqli_query($conn, "INSERT INTO notif VALUES(NULL, '$author', '$title', '$date')");
-    }
+   if ($post) {
+      mysqli_query($conn, "INSERT INTO notif VALUES(NULL, '$author', '$title', '$date')");
+   }
 
-    return mysqli_affected_rows($conn);
+   return mysqli_affected_rows($conn);
 }
 
 // delete post
-function deletePost($data)
-{
-    global $conn;
-    $id = $data['id'];
-    $query = "DELETE FROM postingan WHERE id = $id";
-    mysqli_query($conn, $query);
+function deletePost($data) {
+   global $conn;
+   $id = $data['id'];
+   $query = "DELETE FROM postingan WHERE id = $id";
+   mysqli_query($conn, $query);
 
-    return mysqli_affected_rows($conn);
+   return mysqli_affected_rows($conn);
 }
 
 // change username
-function changeUsername($data)
-{
-    global $conn;
-    $id = $data['id'];
-    $usernameLama = $data['usernameLama'];
-    $usernameBaru = htmlspecialchars($data['usernameBaru']);
+function changeUsername($data) {
+   global $conn;
+   $id = $data['id'];
+   $usernameLama = $data['usernameLama'];
+   $usernameBaru = htmlspecialchars($data['usernameBaru']);
 
-    $query = "UPDATE multi_user SET username = '$usernameBaru' WHERE id = $id";
+   $query = "UPDATE multi_user SET username = '$usernameBaru' WHERE id = $id";
 
-    $changeUsername = mysqli_query($conn, $query);
+   $changeUsername = mysqli_query($conn, $query);
 
-    if ($changeUsername) {
-        mysqli_query($conn, "UPDATE postingan SET author = '$usernameBaru' WHERE author = '$usernameLama' ");
-        mysqli_query($conn, "UPDATE profile SET name = '$usernameBaru' WHERE name = '$usernameLama'");
-        mysqli_query($conn, "UPDATE notif SET author = '$usernameBaru' WHERE author = '$usernameLama'");
-    }
+   if ($changeUsername) {
+      mysqli_query($conn, "UPDATE postingan SET author = '$usernameBaru' WHERE author = '$usernameLama' ");
+      mysqli_query($conn, "UPDATE profile SET name = '$usernameBaru' WHERE name = '$usernameLama'");
+      mysqli_query($conn, "UPDATE notif SET author = '$usernameBaru' WHERE author = '$usernameLama'");
+   }
 
-    return mysqli_affected_rows($conn);
+   return mysqli_affected_rows($conn);
 }
 
+
+// change password
+function changePassword($data) {
+   global $conn;
+   $id = $data['id'];
+   $password = password_hash(strtolower($data['password']),PASSWORD_DEFAULT);
+   $query = "UPDATE multi_user SET password='$password' WHERE id = $id";
+   mysqli_query($conn, $query);
+   return mysqli_affected_rows($conn);
+}
+
+
+
 /* logika request category */
-function send_requestCategory($data)
-{
-  global $conn;
-  $username = htmlspecialchars($data["username"]);
-  $reason = htmlspecialchars($data["reason"]);
-  $category = htmlspecialchars($data["categoryName"]);
-  $date = date("F j, Y, g:i a");
-  /* table require category */
-  $tablename = "request_category";
-  
-  $existCategory = mysqli_query($conn, "SELECT * FROM kategori WHERE kategori = '$category' ");
-  $requestSame = mysqli_query($conn, "SELECT * FROM $tablename WHERE category = '$category' AND username = '$username'");
-  /* cek jika sudah ada category yg sama */
-  if ($existCategory->num_rows > 0) {
-    return [
-       "ok" => false,
-       "message" => "category <b>$category</b> sudah ada!"
-     ];
-  }
-  if ($requestSame->num_rows > 0) {
-    return [
-       "ok" => false,
-       "message" => "ada sudah pernah meminta category <b>$category</b>!"
-     ];
-  }
-  $tableExist = mysqli_query($conn, "SHOW TABLES LIKE '$tablename'");
-  
-  if ($tableExist->num_rows === 0) {
-     if (!mysqli_query($conn, "
-         CREATE TABLE $tablename (
-           id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-           username TEXT NOT NULL,
-           category TEXT NOT NULL,
-           reason TEXT NOT NULL,
-           date TEXT NOT NULL
-         );
-         ")
-     ) {
-       return [
-           "ok" => false,
-           "message" => "can't create into database"
+function send_requestCategory($data) {
+   global $conn;
+   $username = htmlspecialchars($data["username"]);
+   $reason = htmlspecialchars($data["reason"]);
+   $category = htmlspecialchars($data["categoryName"]);
+   $date = date("F j,
+   Y,
+   g:i a");
+   /* table require category */
+   $tablename = "request_category";
+
+   $existCategory = mysqli_query($conn,
+      "SELECT * FROM kategori WHERE kategori = '$category' ");
+   $requestSame = mysqli_query($conn,
+      "SELECT * FROM $tablename WHERE category = '$category' AND username = '$username'");
+   /* cek jika sudah ada category yg sama */
+   if ($existCategory->num_rows > 0) {
+      return [
+         "ok" => false,
+         "message" => "category < b > $category < /b > sudah ada!"
+      ];
+   }
+   if ($requestSame->num_rows > 0) {
+      return [
+         "ok" => false,
+         "message" => "ada sudah pernah meminta category < b > $category < /b>!"
+      ];
+   }
+   $tableExist = mysqli_query($conn, "SHOW TABLES LIKE '$tablename'");
+
+   if ($tableExist->num_rows === 0) {
+      if (!mysqli_query($conn, "
+   CREATE TABLE $tablename (
+      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      username TEXT NOT NULL,
+      category TEXT NOT NULL,
+      reason TEXT NOT NULL,
+      date TEXT NOT NULL
+   );
+   ")
+      ) {
+         return [
+            "ok" => false,
+            "message" => "can't create into database"
          ];
-    }
-  }
-  
-  if (!mysqli_query($conn, "
+      }
+   }
+
+   if (!mysqli_query($conn, "
       INSERT INTO $tablename VALUES (
         NULL,
         '$username',
@@ -207,13 +220,13 @@ function send_requestCategory($data)
         '$date'
       );
     ")) {
-    return [
-       "ok" => false,
-       "message" => "internal server error!"
-     ];
-  }
-  return [
-       "ok" => true,
-       "message" => "success sending request!"
-     ];
+      return [
+         "ok" => false,
+         "message" => "internal server error!"
+      ];
+   }
+   return [
+      "ok" => true,
+      "message" => "success sending request!"
+   ];
 }
